c66c7e818346b78c48290813c6883f28b45772da
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-11-18 16:45:22 -0500

    Change filters of select to text

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,47 +1,3 @@
-// import React from "react";
-// import "../styles/Filters.css";
-//
-// export const Filters = ({ filters, setFilters }) => {
-//   const updateFilter = (e) => {
-//     const { name, value } = e.target;
-//     setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
-//   };
-//
-//   return (
-//     <div className="filters">
-//       <select name="status" value={filters.status} onChange={updateFilter}>
-//         <option value="">All Status</option>
-//         <option value="alive">Alive</option>
-//         <option value="dead">Dead</option>
-//         <option value="unknown">Unknown</option>
-//       </select>
-//
-//       <select name="gender" value={filters.gender} onChange={updateFilter}>
-//         <option value="">All Genders</option>
-//         <option value="male">Male</option>
-//         <option value="female">Female</option>
-//         <option value="genderless">Genderless</option>
-//         <option value="unknown">Unknown</option>
-//       </select>
-//
-//       <select name="species" value={filters.species} onChange={updateFilter}>
-//         <option value="">All Species</option>
-//         <option value="Human">Human</option>
-//         <option value="Alien">Alien</option>
-//         <option value="Humanoid">Humanoid</option>
-//         <option value="Poopybutthole">Poopybutthole</option>
-//         <option value="Mythological Creature">Mythological Creature</option>
-//         <option value="Animal">Animal</option>
-//         <option value="Robot">Robot</option>
-//         <option value="Cronenberg">Cronenberg</option>
-//         <option value="Disease">Disease</option>
-//         <option value="unknown">Unknown</option>
-//       </select>
-//     </div>
-//   );
-// };
-
-
 import React from "react";
 import "../styles/Filters.css";
 
@@ -88,3 +44,48 @@ export const Filters = ({ filters, setFilters }) => {
     </div>
   );
 };
+
+///////////////////////////////////// Filtro con selects ////////////////////////////////////////////
+
+// import React from "react";
+// import "../styles/Filters.css";
+//
+// export const Filters = ({ filters, setFilters }) => {
+//   const updateFilter = (e) => {
+//     const { name, value } = e.target;
+//     setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
+//   };
+//
+//   return (
+//     <div className="filters">
+//       <select name="status" value={filters.status} onChange={updateFilter}>
+//         <option value="">All Status</option>
+//         <option value="alive">Alive</option>
+//         <option value="dead">Dead</option>
+//         <option value="unknown">Unknown</option>
+//       </select>
+//
+//       <select name="gender" value={filters.gender} onChange={updateFilter}>
+//         <option value="">All Genders</option>
+//         <option value="male">Male</option>
+//         <option value="female">Female</option>
+//         <option value="genderless">Genderless</option>
+//         <option value="unknown">Unknown</option>
+//       </select>
+//
+//       <select name="species" value={filters.species} onChange={updateFilter}>
+//         <option value="">All Species</option>
+//         <option value="Human">Human</option>
+//         <option value="Alien">Alien</option>
+//         <option value="Humanoid">Humanoid</option>
+//         <option value="Poopybutthole">Poopybutthole</option>
+//         <option value="Mythological Creature">Mythological Creature</option>
+//         <option value="Animal">Animal</option>
+//         <option value="Robot">Robot</option>
+//         <option value="Cronenberg">Cronenberg</option>
+//         <option value="Disease">Disease</option>
+//         <option value="unknown">Unknown</option>
+//       </select>
+//     </div>
+//   );
+// };
